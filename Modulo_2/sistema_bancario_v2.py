872afa086affad4797a8a6d7b6df13da08a1ba4b
Pedro_Prado <pedro.paiva@inatel.br>
2023-12-21 08:54:43 -0300

    function implementation

--- Modulo_2/sistema_bancario_v2.py
+++ Modulo_2/sistema_bancario_v2.py
@@ -1,3 +1,40 @@
+def deposito(saldo_atual, extrato_atual):
+    deposito = float(input(f'Valor a ser Depositado:'))
+
+    if (deposito > 0):
+        saldo_atual += deposito
+        extrato_atual += f"""  Deposito - R${deposito:.2f} \n"""
+        print(f'Deposito - R${deposito:.2f} realizado com sucesso')
+        return saldo_atual, extrato_atual
+    else:
+        print(f'Deposito inválido')
+        return saldo_atual, extrato_atual
+
+def saque(saldo_atual, extrato_atual, numero_de_saques, limite_de_saques):
+    saque = float(input(f'Valor a ser Sacado:'))
+
+    if saque <= saldo and num_de_saques < LIMITE_SAQUES and saque <= LIMITE:
+        saldo_atual -= saque
+        numero_de_saques += 1
+        extrato_atual += f"""  Saque - R${saque:.2f} \n"""
+        print(f'Saque - R${saque:.2f} realizado com sucesso')
+
+        return saldo_atual, extrato_atual, numero_de_saques
+
+    elif num_de_saques >= limite_de_saques:
+        print(f'Numero de saques excedidos')
+        return saldo_atual, extrato_atual, numero_de_saques
+
+    elif saque > LIMITE:
+        print(f'Saque maior que o limite permitido de R$500.00')
+        return saldo_atual, extrato_atual, numero_de_saques
+
+def verifica_extrato(extrato):
+    print(f'\n    ## EXTRATO ##   ')
+    print(extrato)
+    print(f'Saldo - R${saldo:.2f}')
+
+
 menu = """
         ### Menu ###
     
@@ -19,34 +56,13 @@ while True:
     opcao = int(input(menu))
 
     if opcao == 1:
-        deposito = float(input())
-
-        if(deposito > 0):
-            saldo += deposito
-            extrato += f"""  Deposito - R${deposito:.2f} \n"""
-            print(f'Deposito - R${deposito:.2f} realizado com sucesso')
-        else:
-            print(f'Deposito inválido')
+        saldo, extrato = deposito(saldo_atual=saldo, extrato_atual=extrato)
 
     elif opcao == 2:
-        saque = float(input())
-
-        if saque <= saldo and num_de_saques < LIMITE_SAQUES and saque <= LIMITE:
-            saldo -= saque
-            num_de_saques += 1
-            extrato += f"""  Saque - R${saque:.2f} \n"""
-            print(f'Saque - R${saque:.2f} realizado com sucesso')
-
-        elif num_de_saques >= LIMITE_SAQUES:
-            print(f'Numero de saques excedidos')
-
-        elif saque > LIMITE:
-            print(f'Saque maior que o limite permitido de R$500.00')
+        saldo, extrato, num_de_saques = saque(saldo_atual=saldo, extrato_atual=extrato, numero_de_saques=num_de_saques, limite_de_saques=LIMITE_SAQUES)
 
     elif opcao == 3 :
-        print(f'\n    ## EXTRATO ##   ')
-        print(extrato)
-        print(f'Saldo - R${saldo:.2f}')
+        verifica_extrato(extrato)
 
     elif opcao == 4 :
         print(f'Saldo - R${saldo:.2f}')
