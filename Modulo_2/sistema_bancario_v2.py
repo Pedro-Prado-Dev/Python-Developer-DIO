ba19177018b8f73a4611342dc09db78c75caa0a4
Pedro-Prado-Dev <pedroppaiva1@hotmail.com>
2023-12-21 21:06:34 -0300

    adding a new options and bug-fix

--- Modulo_2/sistema_bancario_v2.py
+++ Modulo_2/sistema_bancario_v2.py
@@ -10,6 +10,7 @@ def deposito(saldo_atual, extrato_atual,/):
         print(f'Deposito inválido')
         return saldo_atual, extrato_atual
 
+
 def saque(*,saldo_atual, extrato_atual, numero_de_saques, limite_de_saques):
     saque = float(input(f'Valor a ser Sacado:'))
 
@@ -29,37 +30,81 @@ def saque(*,saldo_atual, extrato_atual, numero_de_saques, limite_de_saques):
         print(f'Saque maior que o limite permitido de R$500.00')
         return saldo_atual, extrato_atual, numero_de_saques
 
+
 def verifica_extrato(saldo_atual,/,*, extrato):
     print(f'\n    ## EXTRATO ##   ')
     print(extrato)
     print(f'Saldo - R${saldo_atual:.2f}')
 
-def cadastra_usuario(usuarios):
+
+def cadastra_usuario(usuarios_atual):
     usuario = {}
     
     nome = input("Entre com o nome: ")
     usuario["nome"] = nome
     data_de_nasc = input("Entre com a data de nescimento: ")
     usuario["data de nascimento"] = data_de_nasc
-    cpf = int(input("Entre com o cpf: "))
+    cpf = int(input("Entre com o CPF: "))
     
-    for usuario in usuarios:
+    for usuario in usuarios_atual:
         print(usuario["cpf"])
         if usuario["cpf"] == cpf:
             print("Esse cpf já está cadastrado")
-            return usuarios
+            return usuarios_atual
     
     usuario["cpf"] = cpf
     endereço = """"""
     logadouro = input("Entre com  o Logadouro: ")
     bairro = input("Entre com  o Bairro: ")
-    cidade = input("Entre com  a cidade: ")
+    cidade = input("Entre com  a Cidade: ")
     estado = input("Entre com  o Estado: ")
     endereço += f"""Logadouro - {logadouro}, bairro {bairro}, cidade = {cidade}, estado - {estado}"""
     usuario["endereço"] = endereço
-    usuarios.append(usuario)
+    usuarios_atual.append(usuario)
     
+    return usuarios_atual
+
+
+def mostra_usuarios(usuarios_atual):
+    for usuario in usuarios_atual:
+        for chave, valor in usuario.items():
+            print(f'{chave} - {valor}')
+        print('\n')
+
+
+def cadastra_conta(contas_atual, usuarios_atual):
+    conta = {}
+    conta["agencia"] = "0001"
     
+    if len(contas_atual) == 0:
+        conta["numero da conta"] = 1
+    else:
+        ultima_conta = contas_atual[len(contas_atual) - 1]
+        conta["numero da conta"] = ultima_conta["numero da conta"] + 1
+    
+    cpf = int(input("Entre com o CPF: "))
+    flag_cpf = True
+    for usuario in usuarios_atual:
+        
+        if usuario["cpf"] == cpf:
+            flag_cpf = False
+            break
+        
+    if flag_cpf == True:
+        print(f'CPF não cadastrado cadastre o usuario primeiro')
+        return contas_atual
+    else:
+        conta["cpf"] = cpf
+        contas_atual.append(conta)
+        return contas_atual
+
+
+def mostra_contas(contas_atual):
+    for conta in contas_atual:
+        for chave, valor in conta.items():
+            print(f'{chave} - {valor}')
+        print('\n')
+
 
 menu = """
         ### Menu ###
@@ -68,7 +113,10 @@ menu = """
     (2) - Sacar
     (3) - Extrato
     (4) - Cadastra Usuario
-    (6) - Sair
+    (5) - Mostra Usuarios
+    (6) - Cadastra Conta
+    (7) - Mostra Conta
+    (8) - Sair
 
 """
 
@@ -78,6 +126,7 @@ num_de_saques = 0
 LIMITE_SAQUES = 3
 LIMITE = 500
 usuarios = []
+contas = []
 
 
 while True:
@@ -93,10 +142,18 @@ while True:
         verifica_extrato(saldo, extrato=extrato)
     
     elif opcao == 4 :
-        cadastra_usuario(usuarios)
-        print(usuarios)
-
+        usuarios = cadastra_usuario(usuarios)
+        
+    elif opcao == 5 :
+        mostra_usuarios(usuarios)
+    
     elif opcao == 6 :
+        contas = cadastra_conta(contas, usuarios)
+    
+    elif opcao == 7 :
+        mostra_contas(contas)
+    
+    elif opcao == 8 :
         print(f'Saldo - R${saldo:.2f}')
         break
     else:
