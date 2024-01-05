3e0f9dee932b8ffc29587770dab3869b49be3f12
Pedro-Prado-Dev <pedroppaiva1@hotmail.com>
2024-01-04 22:18:42 -0300

    adding a new class

--- Modulo_3/sistema_bancario_poo/sistema_bancario_v3.py
+++ Modulo_3/sistema_bancario_poo/sistema_bancario_v3.py
@@ -20,7 +20,48 @@ class PessoaFisica(Cliente):
         self.cpf = cpf
 
 class Conta:
-    pass
+    def __init__(self, numero, cliente) -> None:
+        self._saldo = 0
+        self._numero = numero
+        self._agencia = "0001"
+        self._cliente = cliente
+        self._historico = Historico()
+    
+    @classmethod
+    def nova_conta(cls, cliente, numero):
+        return cls(numero, cliente)
+    
+    @property
+    def saldo(self):
+        return self._saldo
+    
+    @property
+    def numero(self):
+        return self._numero
+    
+    @property
+    def agencia(self):
+        return self._agencia
+    
+    @property
+    def cliente(self):
+        return self._cliente
+    
+    @property
+    def historico(self):
+        return self._historico
+    
+    def sacar(self, valor):
+        saldo = self.saldo
+        if valor > saldo :
+            print(f"\n Falha! Seu saldo é insuficiente")
+        
+        elif valor > 0:
+            self._saldo += valor
+            print(f"\n Saque no valor R${valor} realizado com sucesso")
+        
+        else:
+            print(f" Valor informado para saque invalido")
 
 class ContaCorrente(Conta):
     pass
