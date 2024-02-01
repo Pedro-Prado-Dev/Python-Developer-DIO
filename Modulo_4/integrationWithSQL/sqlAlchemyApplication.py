37863428f3105774a86242158688a89520363a0e
Pedro-Prado-Dev <pedroppaiva1@hotmail.com>
2024-01-31 22:32:24 -0300

    sqlAlchemy creating a small relationship

---
+++ Modulo_4/integrationWithSQL/sqlAlchemyApplication.py
@@ -0,0 +1,31 @@
+import sqlalchemy
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user_account'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    full_name = Column(String)
+
+    address = relationship(
+        'Address', back_populates='user', cascade='all, delete-orphan'
+    )
+
+    def __repr__(self):
+        return f'User (id={self.id}, name={self.name}, full_name={self.full_name})'
+
+class Address(Base):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email_address = Column(String(50), unique=True, nullable=False)
+    user_id = Column(Integer, ForeignKey('user_account.id'), nullable=False)
+
+    user = relationship(
+        'User', back_populates='addresses'
+    )
+
+    def __repr__(self):
+        return f'Address (id={self.id}, email={self.email_address})'
